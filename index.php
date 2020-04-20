57c4551822c5758d8ce2e42a0ae2a99370c5b3a0
Sabrinahedyafadillahputri <53220754+Sabrinahedyafadillahputri@users.noreply.github.com>
2020-04-21 01:09:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+include 'aset/header.php';
+ ?>
+
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Index SiPERPUS</title>
+    </head>
+    <body>
+         <div class="container">
+ <div class="row mt-4">
+ <div class="col-md">
+            <h2><i class="fas fa-chart-line mr-2"></i>Dashboard</h2>
+            <hr class="bg-light">
+
+ </div>
+ </div>
+
+ <div class="row">
+    <div class="col-md-4">
+    <div class="card bg-danger" style="width: 18rem;">
+    <div class="card-body text-white">
+    <h5 class="card-title">Jumlah buku</h5>
+    <p class="card-text" style="font-size: 60px">350</p>
+    <a href="#" class="text-white">lebih detail<i class="fas fa-angle-double-right"></i></a>
+    </div>
+    </div>
+    </div>
+   <div class="col-md-4">
+   <div class="card bg-warning" style="width: 18rem;">
+   <div class="card-body text-white">
+    <h5 class="card-title">Jumlah anggota</h5>
+    <p class="card-text" style="font-size: 60px">350</p>
+    <a href="#" class="text-white">lebih detail<i class="fas fa-angle-double-right"></i></a>
+    </div>
+    </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card bg-info" style="width: 18rem;">
+  <div class="card-body text-white">
+    <h5 class="card-title">Jumlah transaksi</h5>
+    <p class="card-text" style="font-size: 60px">350</p>
+    <a href="#" class="text-white">lebih detail<i class="fas fa-angle-double-right"></i></a>
+    </div>
+    </div>
+    </div>
+    </body>
+    </html>    
+
+ <?php 
+include 'aset/footer.php';
+ ?>
